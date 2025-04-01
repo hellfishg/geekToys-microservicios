5fa1bd43c66a5b2ad0af48541980815afa8fd460
hellfishg <hellfish.g@protonmail.com>
2025-04-01 16:36:51 +0200

    - Pedidos-services levantada en docker y con coneccion de DB. - Integracion de pedidos-service a discovery-service.

--- geektoys-pedido-service/src/main/java/io/geekToys/geektoys_pedido_service/GeekToysPedidoServiceApplication.java
+++ geektoys-pedido-service/src/main/java/io/geekToys/geektoys_pedido_service/GeekToysPedidoServiceApplication.java
@@ -3,6 +3,7 @@ package io.geekToys.geektoys_pedido_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class GeekToysPedidoServiceApplication {
 
