90ac75ab4a0efa6cd233b19f841a8adbfee6ad1f
hellfishg <hellfish.g@protonmail.com>
2025-04-07 16:01:11 +0200

    Integración de Kafka en pedido-service - Se crea comunicacion producer <-> consumer.

--- geektoys-pedido-service/src/main/java/io/geekToys/geektoys_pedido_service/controller/PedidoController.java
+++ geektoys-pedido-service/src/main/java/io/geekToys/geektoys_pedido_service/controller/PedidoController.java
@@ -1,5 +1,6 @@
 package io.geekToys.geektoys_pedido_service.controller;
 
+import io.geekToys.geektoys_pedido_service.producer.PedidoProducer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,16 +9,16 @@ import java.util.List;
 
 @RestController()
 public class PedidoController {
-//    PedidoProducer pedidoProducer;
-//
-//    public PedidoController(PedidoProducer pedidoProducer){
-//        this.pedidoProducer = pedidoProducer;
-//    }
+    PedidoProducer pedidoProducer;
+
+    public PedidoController(PedidoProducer pedidoProducer){
+        this.pedidoProducer = pedidoProducer;
+    }
 
     @GetMapping("/order-request")
     public ResponseEntity<List<String>> orderRequest(){
         String message = "Order Request";
-        //pedidoProducer.sendMessage(message);
+        pedidoProducer.sendMessage(message);
         System.out.println("Message sent --> " + message);
         return ResponseEntity.ok(List.of(message));
     }
